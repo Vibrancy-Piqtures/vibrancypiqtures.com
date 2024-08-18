2f2f39bd87feb29e930816919dcacdc9ad97fc82
Marq-Director <168653119+Marq-Director@users.noreply.github.com>
2024-08-18 20:39:13 +0300

    update all

--- script/script4.js
+++ script/script4.js
@@ -24,3 +24,17 @@ document.querySelectorAll('.accordion-button').forEach(button => {
     });
   });
   
+
+  window.addEventListener("scroll", function() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (window.scrollY > window.innerHeight) {  // Adjusted to 1x the viewport height
+      scrollToTopBtn.classList.add("show");
+    } else {
+      scrollToTopBtn.classList.remove("show");
+    }
+  });
+  
+  document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  

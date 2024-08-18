9a08fe4624ae0a734dc4f458e9dd331207edbbb8
Marq-Director <168653119+Marq-Director@users.noreply.github.com>
2024-08-18 16:08:42 +0300

    update all

--- script/script4.js
+++ script/script4.js
@@ -3,3 +3,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     $("#navbar-placeholder").load("Navbar/navbar.html"); 
 });
+
+document.querySelectorAll('.accordion-button').forEach(button => {
+    button.addEventListener('click', () => {
+      const accordionItem = button.parentElement;
+      const content = accordionItem.querySelector('.accordion-content');
+  
+      // Toggle the active class
+      if (accordionItem.classList.contains('active')) {
+        accordionItem.classList.remove('active');
+        content.style.maxHeight = 0;
+      } else {
+        document.querySelectorAll('.accordion-item').forEach(item => {
+          item.classList.remove('active');
+          item.querySelector('.accordion-content').style.maxHeight = 0;
+        });
+        accordionItem.classList.add('active');
+        content.style.maxHeight = content.scrollHeight + 'px';
+      }
+    });
+  });
+  

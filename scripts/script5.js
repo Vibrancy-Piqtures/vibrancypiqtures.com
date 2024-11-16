4c40f807015d15fb52878ad3842c6637cc7b0903
Marq-Director <markdirector000@gmail.com>
2024-11-16 13:31:55 +0300

    update

--- scripts/script5.js
+++ scripts/script5.js
@@ -3,3 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     $("#navbar-placeholder").load("Navbar/navbar.html");
   });
   
+  // Scroll to top button
+window.addEventListener("scroll", function () {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (window.scrollY > window.innerHeight) {
+    // Adjusted to 1x the viewport height
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+});
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
